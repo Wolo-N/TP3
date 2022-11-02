aea3c4cf7d7f40ab7bf5fb86056d4607cfeecd49
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2022-11-02 17:39:07 -0300

    con test 5b

--- Test5a.asm
+++ Test5a.asm
@@ -5,10 +5,9 @@ main:
     SET R7, 0xFF
     PUSH |R7|, R0
     PUSH |R7|, R1
-    DB 0x77 
+    DB 0x7F 
     DB 0x00
     POP |R7|, R1
     POP |R7|, R0
-
 halt:
 	JMP halt
---
+++ Test5b.asm
@@ -0,0 +1,6 @@
+main:
+    SET R0, 0x3A
+    DB 0x70 
+    DB 0x00
+halt:
+	JMP halt
