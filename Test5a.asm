74f4876e88262d9da5d46e562029599b33cc7b84
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2022-11-02 18:50:49 -0300

    - update

--- Test5a.asm
+++ Test5a.asm
@@ -1,13 +1,13 @@
-
+; 01111 111 011 00000
 main:
     SET R0, 0xA2
     SET R1, 0x01
     SET R7, 0xFF
     PUSH |R7|, R0
     PUSH |R7|, R1
-    DB 0x7F 
-    DB 0x00
-    POP |R7|, R1
-    POP |R7|, R0
+    ; POPADD |R7|, R3
+    DB 0x7F
+    DB 0x60
+
 halt:
 	JMP halt
